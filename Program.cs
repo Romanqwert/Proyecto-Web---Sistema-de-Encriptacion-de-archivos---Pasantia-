771dcafbedf1a5c958d450fc3e73eda038680a31
fmadera <fmadera@cerberussolutions.io>
2025-10-22 16:18:30 -0400

    feat: endpoints de usuario

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using EncriptacionApi.Data;
 using Microsoft.EntityFrameworkCore;
 using DotNetEnv;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace EncriptacionApi
 {
@@ -9,6 +12,9 @@ namespace EncriptacionApi
     {
         public static void Main(string[] args)
         {
+            // Cargar variables del .env
+            Env.Load();
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
@@ -19,14 +25,32 @@ namespace EncriptacionApi
             builder.Services.AddSwaggerGen();
 
             // Conexion a la base de datos
-            Env.Load();
-
             builder.Services.AddDbContext<AppDbContext>(options => 
                 options.UseMySql(
                     Environment.GetEnvironmentVariable("DB_CONNECTION"),
                     new MySqlServerVersion(new Version(8, 0, 36))
                 )
             );
+
+            // JWT
+            var jwtKey = Environment.GetEnvironmentVariable("JWT_SECRET");
+            var jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
+            var jwtAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE");
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtIssuer,
+                        ValidAudience = jwtAudience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                    };
+                });
 
             var app = builder.Build();
 
